08986ba6532d72f920c1f823879ee18484fe5235
Lukasz Samson <lukaszsamson@gmail.com>
2016-07-08 18:19:50 +0200

    Append respects max version

--- main.erl
+++ main.erl
@@ -1,14 +1,25 @@
 -module(main).
 -export([stream/1]).
 
+isVersionOk(Version, MaxVersion) ->
+  MaxVersion < 0 orelse Version =< MaxVersion.
+
 stream(Events) ->
   io:format("Listening~n", []),
+  Version = length(Events),
   receive
     {From, getEvents} ->
       io:format("getEvents rec~n", []),
       From ! Events,
       stream(Events);
-    {_From, appendEvents, NewEvents} ->
+    {From, appendEvents, NewEvents, MaxVersion} ->
       io:format("appendEvents rec~n", []),
-      stream(Events ++ NewEvents);
+      case isVersionOk(Version, MaxVersion) of
+        true ->
+          From ! ack,
+          stream(Events ++ NewEvents);
+        false ->
+          From ! concurrencyError,
+          stream(Events)
+      end
   end.
--- main_tests.erl
+++ main_tests.erl
@@ -17,18 +17,53 @@ getEvents_returns_initial_empty_test() ->
 getEvents_returns_initial_nonempty_test() ->
   getEvents_returns_initial([2, 3, 5]).
 
-appendEvents(InitialEvents, NewEvents) ->
+appendEvents(InitialEvents, NewEvents, ExpectedEvents, MaxVersion) ->
   S = spawn(main, stream, [InitialEvents]),
-  S ! {self(), appendEvents, NewEvents},
+  S ! {self(), appendEvents, NewEvents, MaxVersion},
+  receive
+    _ -> ok
+  end,
   S ! {self(), getEvents},
   receive
     Events ->
-      ?assert(Events =:= InitialEvents ++ NewEvents)
+      ?assert(Events =:= ExpectedEvents)
   end,
   ok.
 
 appendEvents_empty_test() ->
-  appendEvents([], [5]).
+  appendEvents([], [5], [5], -1).
 
 appendEvents_nonempty_test() ->
-  appendEvents([2, 3], [5]).
+  appendEvents([2, 3], [5], [2, 3, 5], -1).
+
+appendEvents_nonempty_should_not_append_if_wrong_version_test() ->
+  appendEvents([2, 3], [5], [2, 3], 1).
+
+appendEventsVersionCheck(InitialEvents, NewEvents, MaxVersion, ExpectedResult) ->
+  S = spawn(main, stream, [InitialEvents]),
+  S ! {self(), appendEvents, NewEvents, MaxVersion},
+  receive
+    Result -> ?assert(Result =:= ExpectedResult)
+  end,
+  ok.
+
+appendEvents_empty_anyVersion_test() ->
+  appendEventsVersionCheck([], [5], -1, ack).
+
+appendEvents_empty_exactVersion_test() ->
+  appendEventsVersionCheck([], [5], 0, ack).
+
+appendEvents_empty_greaterVersion_test() ->
+  appendEventsVersionCheck([], [5], 1, ack).
+
+appendEvents_nonempty_anyVersion_test() ->
+  appendEventsVersionCheck([2, 3], [5], -1, ack).
+
+appendEvents_nonempty_exactVersion_test() ->
+  appendEventsVersionCheck([2, 3], [5], 2, ack).
+
+appendEvents_nonempty_greaterVersion_test() ->
+  appendEventsVersionCheck([2, 3], [5], 3, ack).
+
+appendEvents_nonempty_lowerVersion_test() ->
+  appendEventsVersionCheck([2, 3], [5], 1, concurrencyError).
